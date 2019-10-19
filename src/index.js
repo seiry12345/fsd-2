17d2040be34b009817201a382b854078b013d69e
seiry12345 <seiry@mail.ru>
2019-10-19 19:25:00 +0600

    restructuring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 // libs
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// window.Vue = require('vue');
 
 import './js/common';
 import './assets/css/main.css';
 import './assets/scss/main.scss';
-
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// window.Vue = require('vue');
