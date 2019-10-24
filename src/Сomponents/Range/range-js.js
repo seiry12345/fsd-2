b11f9b34175d9f13fab41e2c6e876af94e45083e
seiry12345 <seiry@mail.ru>
2019-10-24 21:25:54 +0600

    range slider io completed

--- src/Сomponents/Range/range-js.js
+++ src/Сomponents/Range/range-js.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
-  $('.js-range-slider').ionRangeSlider({
-    type: "double",
-  });
+const numbers = $('.label-right__number');
+const min = $('.label-right__number-min');
+const max = $('.label-right__number-max');
+
+// format to ru
+function formatNumber (numb) {
+  return Number(numb, 10).toLocaleString('ru');
+}
+
+// format on init
+numbers.each((i, numb) => {
+  let numbText = $(numb).text();
+  $(numb).text(formatNumber(numbText) + "₽");
+});
+
+
+$('.js-range-slider').ionRangeSlider({
+  type: "double",
+  skin: "round",
+  onChange: function (data) {
+    min.text(formatNumber(data.from) + "₽");
+    max.text(formatNumber(data.to) + "₽");
+  }
 });
