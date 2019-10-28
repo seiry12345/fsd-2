153b4878bf213c8bb1b91d1ac2cee4ea24b42ea3
seiry12345 <seiry@mail.ru>
2019-10-28 21:19:24 +0600

    date filter

--- src/Сomponents/Dropdown-date/dropdown-date.js
+++ src/Сomponents/Dropdown-date/dropdown-date.js
@@ -1,1 +1,30 @@
-$('.datepicker-here').inputmask({alias: "99.99.9999"})
+const dropdownDate = $('.dropdown-date__item .dropdown-date__input')
+const dropdownRange = $('.dropdown-date__item-range .dropdown-date__input')
+const dropdownFilter = $('.dropdown-date__item-filter .dropdown-date__input')
+
+// mask to all dates
+if (dropdownDate.length) {
+  dropdownDate.inputmask({alias: '99.99.9999'})
+}
+
+// range dates
+if (dropdownRange.length) {
+  dropdownRange.datepicker({
+    range: true,
+    onSelect: function(fd, d, picker) {
+      $('.dropdown-date__start').val(fd.split(',')[0])
+      $('.dropdown-date__end').val(fd.split(',')[1])
+    },
+  })
+}
+
+// filtered dates
+if (dropdownFilter.length) {
+  dropdownFilter.datepicker({
+    range: true,
+    onSelect: function(fd, d, picker) {
+      console.log(d,
+        picker)
+    }
+  })
+}
