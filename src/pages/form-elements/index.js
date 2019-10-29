f61b4b6f0202b3b91c7e58d35feca5e9652fb9d1
seiry12345 <seiry@mail.ru>
2019-10-29 23:30:52 +0600

    mask for dropdown date

--- src/pages/form-elements/index.js
+++ src/pages/form-elements/index.js
@@ -16,25 +16,15 @@ import '../../Сomponents/Rate-btn/rate-btn.scss';
 import '../../Сomponents/Toggle/toggle.scss';
 import '../../Сomponents/Button/button.scss';
 import '../../Сomponents/Pagination/pagination.scss';
+import '../../Сomponents/Dropdown-date/dropdown-date.scss'
+import '../../Сomponents/Dropdown-date/dropdown-date'
+import '../../Сomponents/Like-btn/like-btn';
 
 // jquery
 import 'jquery/dist/jquery.min'
-
-// like btn
-import '../../Сomponents/Like-btn/like-btn';
 
 // ion range slider
 import 'ion-rangeslider/css/ion.rangeSlider.min.css'
 import '../../Сomponents/Range/range.scss';
 import 'ion-rangeslider/js/ion.rangeSlider.min'
 import '../../Сomponents/Range/range-js'
-
-// mask date
-import 'inputmask/css/inputmask.css'
-import 'inputmask/dist/inputmask/jquery.inputmask'
-
-// date picker
-import 'air-datepicker/dist/css/datepicker.min.css'
-import '../../Сomponents/Dropdown-date/dropdown-date.scss'
-import 'air-datepicker/dist/js/datepicker.min'
-import '../../Сomponents/Dropdown-date/dropdown-date'
--- src/Сomponents/Dropdown-date/dropdown-date.js
+++ src/Сomponents/Dropdown-date/dropdown-date.js
@@ -1,12 +1,39 @@
+import 'inputmask/css/inputmask.css'
+import 'inputmask/dist/inputmask/jquery.inputmask'
+import 'inputmask/dist/inputmask/bindings/inputmask.binding'
+
+import 'air-datepicker/dist/css/datepicker.min.css'
+import 'air-datepicker/dist/js/datepicker.min'
+
 const dropdownDate = $('.dropdown-date__item .dropdown-date__input')
 const dropdownRange = $('.dropdown-date__item-range .dropdown-date__input')
-const dropdownFilter = $('.dropdown-date__item-filter .dropdown-date__input')
+const dropdownFiltered = $('.dropdown-date__item-filter .dropdown-date__input')
 
-// mask to all dates
-if (dropdownDate.length) {
-  dropdownDate.inputmask({alias: '99.99.9999'})
-}
 
+// mask
+dropdownDate.inputmask(
+  '99.99.9999',
+  {
+    'placeholder': 'ДД.ММ.ГГГГ',
+  },
+  {
+    'alias': 'datetime',
+  },
+)
+
+dropdownFiltered.inputmask(
+  {
+    'mask': '9{1,2} a{3} - 9{1,2} a{3}',
+  },
+  {
+    'placeholder': 'ДД.МММ - ДД.МММ',
+  },
+  {
+    'alias': 'datetime',
+  },
+)
+
+// air date picker
 // range dates
 if (dropdownRange.length) {
   dropdownRange.datepicker({
@@ -19,12 +46,9 @@ if (dropdownRange.length) {
 }
 
 // filtered dates
-if (dropdownFilter.length) {
-  dropdownFilter.datepicker({
+if (dropdownFiltered.length) {
+  dropdownFiltered.datepicker({
     range: true,
-    onSelect: function(fd, d, picker) {
-      console.log(d,
-        picker)
-    }
+    dateFormat: 'dd/mm/yyyy',
   })
 }
