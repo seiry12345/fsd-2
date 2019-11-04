a6091bb27ef0c22a2478a80ad41bd1e3ececa17d
seiry12345 <seiry@mail.ru>
2019-11-04 17:28:58 +0600

    begin checkbox-expanded

---
+++ src/Сomponents/Like-btn/like-btn.js
@@ -0,0 +1,10 @@
+const btn = $('.form__item-like-btn .like-btn')
+
+btn.click(function(event) {
+  const target = $(event.target)
+  let targetText = parseInt(target.text(), 10)
+  event.preventDefault()
+
+  target.text(targetText + 1)
+  target.addClass('like-btn--disabled')
+})
