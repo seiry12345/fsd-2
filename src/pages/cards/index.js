d630da440ac5f746aae1a5045b921bdbab3f5cf4
seiry12345 <seiry@mail.ru>
2019-11-21 16:19:13 +0600

    rename room to card-room

--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -1,26 +1,34 @@
 // scafolding
-import '../../assets/scss/main.scss';
+import '../../assets/scss/main.scss'
 
 // ui kit logo
-import '../../Сomponents/Ui-logo/ui-logo.scss';
+import '../../Сomponents/Ui-logo/ui-logo.scss'
 
 // elements
-import './index.scss';
-import '../../Сomponents/Input/input.scss';
-import '../../Сomponents/Dropdown/dropdown.scss';
-import '../../Сomponents/Dropdown-date/dropdown-date.scss';
-import '../../Сomponents/Button/button.scss';
-import '../../Сomponents/Radio/radio.scss';
-import '../../Сomponents/Toggle/toggle.scss';
+import './index.scss'
+import '../../Сomponents/Input/input.scss'
+import '../../Сomponents/Dropdown/dropdown.scss'
+import '../../Сomponents/Dropdown-date/dropdown-date.scss'
+import '../../Сomponents/Button/button.scss'
+import '../../Сomponents/Radio/radio.scss'
+import '../../Сomponents/Toggle/toggle.scss'
 
 // cards
 import '../../Сomponents/Cards/cards.scss'
 import '../../Сomponents/Cards/Find-room/find-room.scss'
 import '../../Сomponents/Cards/Registration/registration.scss'
-import '../../Сomponents/Cards/Room/room.scss'
+import '../../Сomponents/Cards/Card-room/card-room.scss'
 import '../../Сomponents/Cards/Auth/auth.scss'
 
 // jquery
-import 'jquery/dist/jquery.min';
-import '../../Сomponents/Dropdown/dropdown';
-import '../../Сomponents/Dropdown-date/dropdown-date';
+import 'jquery/dist/jquery.min'
+import '../../Сomponents/Dropdown/dropdown'
+import '../../Сomponents/Dropdown-date/dropdown-date'
+
+
+// datepicker expanded
+const datepickerExpanded = $('.datepicker--expanded');
+
+if (datepickerExpanded.length > 0) {
+  datepickerExpanded.datepicker()
+}
