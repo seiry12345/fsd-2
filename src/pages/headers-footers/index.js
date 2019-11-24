9ee4ece7df19313aa1886980edd00e3e5b52c8f7
seiry12345 <seiry@mail.ru>
2019-11-24 15:22:48 +0600

    header

---
+++ src/pages/headers-footers/index.js
@@ -0,0 +1,15 @@
+// scafolding
+import '../../assets/scss/main.scss'
+
+// ui kit logo
+import '../../Сomponents/Ui-logo/ui-logo.scss'
+
+// elements
+import './index.scss'
+import '../../Сomponents/Button/button.scss'
+import '../../Сomponents/Navbar/navbar.scss'
+import '../../Сomponents/Header/header.scss'
+import '../../Сomponents/User/user.scss'
+
+// jquery
+import 'jquery/dist/jquery.min'
