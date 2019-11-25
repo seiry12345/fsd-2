181ee7bf78f0dcfd82b6fe104d7a925a6b7a03ee
seiry12345 <seiry@mail.ru>
2019-11-25 16:10:49 +0600

    headers and footers

--- src/pages/headers-footers/index.js
+++ src/pages/headers-footers/index.js
@@ -7,9 +7,15 @@ import '../../Сomponents/Ui-logo/ui-logo.scss'
 // elements
 import './index.scss'
 import '../../Сomponents/Button/button.scss'
+import '../../Сomponents/Input/input.scss'
 import '../../Сomponents/Navbar/navbar.scss'
 import '../../Сomponents/Header/header.scss'
 import '../../Сomponents/User/user.scss'
+import '../../Сomponents/Footer/footer.scss'
+import '../../Сomponents/Navigation/navigation.scss'
+import '../../Сomponents/Subscribe/subscribe.scss'
+import '../../Сomponents/Copyrights/copyrights.scss'
+import '../../Сomponents/Social/social.scss'
 
 // jquery
 import 'jquery/dist/jquery.min'
